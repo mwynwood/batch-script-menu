b6d6d52be597afd3c0e0d28bf04860a1607f7e08
Marcus Wynwood <mwynwood@gmail.com>
2022-03-22 14:59:50 +1100

    Update menu.bat

--- menu.bat
+++ menu.bat
@@ -9,13 +9,23 @@ echo 1. ipconfig
 echo 2. shutdown
 echo 3. Check Disk
 echo 4. Task List
+echo Q. Quit
 set /p Input=Selection:
 If /I "%Input%"=="1" goto option1
+If /I "%Input%"=="2" goto option2
 If /I "%Input%"=="3" goto option3
 If /I "%Input%"=="4" goto option4
+If /I "%Input%"=="Q" goto quit
+echo Invalid input
+goto menu
 
 :option1
 ipconfig
+pause
+goto menu
+
+:option2
+echo Not implemented
 pause
 goto menu
 
@@ -28,3 +38,6 @@ goto menu
 tasklist
 pause
 goto menu
+
+:quit
+cls
